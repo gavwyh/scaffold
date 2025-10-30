6f67f004a2d60dbe92519a8747fd6aa2240be6e1
Template Janitor <template.janitor@example.com>
2025-10-30 04:45:04 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(scaffold_supports_sanitizers)
   if((CMAKE_CXX_COMPILER_ID MATCHES ".*Clang.*" OR CMAKE_CXX_COMPILER_ID MATCHES ".*GNU.*") AND NOT WIN32)
 
     message(STATUS "Sanity checking UndefinedBehaviorSanitizer, it should be supported on this platform")
@@ -54,183 +54,183 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(scaffold_setup_options)
+  option(scaffold_ENABLE_HARDENING "Enable hardening" ON)
+  option(scaffold_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    scaffold_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    scaffold_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  scaffold_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR scaffold_PACKAGING_MAINTAINER_MODE)
+    option(scaffold_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(scaffold_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(scaffold_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(scaffold_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(scaffold_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(scaffold_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(scaffold_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(scaffold_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(scaffold_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(scaffold_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(scaffold_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(scaffold_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(scaffold_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(scaffold_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(scaffold_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(scaffold_ENABLE_USER_LINKER "Enable user-selected linker" OFF)
+    option(scaffold_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(scaffold_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(scaffold_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(scaffold_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(scaffold_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(scaffold_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(scaffold_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(scaffold_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(scaffold_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(scaffold_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_USER_LINKER
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      scaffold_ENABLE_IPO
+      scaffold_WARNINGS_AS_ERRORS
+      scaffold_ENABLE_USER_LINKER
+      scaffold_ENABLE_SANITIZER_ADDRESS
+      scaffold_ENABLE_SANITIZER_LEAK
+      scaffold_ENABLE_SANITIZER_UNDEFINED
+      scaffold_ENABLE_SANITIZER_THREAD
+      scaffold_ENABLE_SANITIZER_MEMORY
+      scaffold_ENABLE_UNITY_BUILD
+      scaffold_ENABLE_CLANG_TIDY
+      scaffold_ENABLE_CPPCHECK
+      scaffold_ENABLE_COVERAGE
+      scaffold_ENABLE_PCH
+      scaffold_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  scaffold_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (scaffold_ENABLE_SANITIZER_ADDRESS OR scaffold_ENABLE_SANITIZER_THREAD OR scaffold_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(scaffold_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(scaffold_global_options)
+  if(scaffold_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    scaffold_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  scaffold_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(scaffold_ENABLE_HARDENING AND scaffold_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR scaffold_ENABLE_SANITIZER_UNDEFINED
+       OR scaffold_ENABLE_SANITIZER_ADDRESS
+       OR scaffold_ENABLE_SANITIZER_THREAD
+       OR scaffold_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${scaffold_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${scaffold_ENABLE_SANITIZER_UNDEFINED}")
+    scaffold_enable_hardening(scaffold_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(scaffold_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(scaffold_warnings INTERFACE)
+  add_library(scaffold_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  scaffold_set_project_warnings(
+    scaffold_warnings
+    ${scaffold_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
     "")
 
-  if(myproject_ENABLE_USER_LINKER)
+  if(scaffold_ENABLE_USER_LINKER)
     include(cmake/Linker.cmake)
-    myproject_configure_linker(myproject_options)
+    scaffold_configure_linker(scaffold_options)
   endif()
 
   include(cmake/Sanitizers.cmake)
-  myproject_enable_sanitizers(
-    myproject_options
-    ${myproject_ENABLE_SANITIZER_ADDRESS}
-    ${myproject_ENABLE_SANITIZER_LEAK}
-    ${myproject_ENABLE_SANITIZER_UNDEFINED}
-    ${myproject_ENABLE_SANITIZER_THREAD}
-    ${myproject_ENABLE_SANITIZER_MEMORY})
+  scaffold_enable_sanitizers(
+    scaffold_options
+    ${scaffold_ENABLE_SANITIZER_ADDRESS}
+    ${scaffold_ENABLE_SANITIZER_LEAK}
+    ${scaffold_ENABLE_SANITIZER_UNDEFINED}
+    ${scaffold_ENABLE_SANITIZER_THREAD}
+    ${scaffold_ENABLE_SANITIZER_MEMORY})
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(scaffold_options PROPERTIES UNITY_BUILD ${scaffold_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(scaffold_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      scaffold_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(scaffold_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    scaffold_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(scaffold_ENABLE_CLANG_TIDY)
+    scaffold_enable_clang_tidy(scaffold_options ${scaffold_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(scaffold_ENABLE_CPPCHECK)
+    scaffold_enable_cppcheck(${scaffold_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(scaffold_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    scaffold_enable_coverage(scaffold_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(scaffold_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(scaffold_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(scaffold_ENABLE_HARDENING AND NOT scaffold_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR scaffold_ENABLE_SANITIZER_UNDEFINED
+       OR scaffold_ENABLE_SANITIZER_ADDRESS
+       OR scaffold_ENABLE_SANITIZER_THREAD
+       OR scaffold_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    scaffold_enable_hardening(scaffold_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
